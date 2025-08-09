bcf19d6409807aed2404ab5eae51bef4a1347adc
Syed Muzakkir Hussain <138936198+muzakkirhussain011@users.noreply.github.com>
2025-08-10 01:58:32 +0530

    Update __init__.py

--- faircare/fairness/__init__.py
+++ faircare/fairness/__init__.py
@@ -1,3 +1,27 @@
 # faircare/fairness/__init__.py
-from .metrics import dp_gap, eo_gap, fpr_gap, calibration_ece, confusion_by_group
-from .global_stats import aggregate_group_stats
+"""
+Public API for fairness helpers.
+
+We re-export the underscored implementations from metrics.py so that
+older imports like `from faircare.fairness import dp_gap` continue to work.
+"""
+
+from .metrics import (
+    _dp_gap as dp_gap,
+    _eo_gap as eo_gap,
+    _fpr_gap as fpr_gap,
+    expected_calibration_error as calibration_ece,
+    _confusion_by_group as confusion_by_group,
+    compute_metrics,
+    threshold_sweep,
+)
+
+__all__ = [
+    "dp_gap",
+    "eo_gap",
+    "fpr_gap",
+    "calibration_ece",
+    "confusion_by_group",
+    "compute_metrics",
+    "threshold_sweep",
+]
