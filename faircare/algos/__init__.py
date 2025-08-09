fe3213204f56db25e9c85b1fd7214c33305b0cff
Syed Muzakkir Hussain <138936198+muzakkirhussain011@users.noreply.github.com>
2025-08-09 20:04:35 +0530

    Create __init__.py

---
+++ faircare/algos/__init__.py
@@ -0,0 +1,9 @@
+# faircare/algos/__init__.py
+from .fedavg import FedAvg
+from .fedprox import FedProx
+from .qffl import QFFL
+from .afl import AFL
+from .fairfed import FairFed
+from .fedgft import FedGFT
+from .fairfate import FairFATE
+from .faircare_fl import FairCareFL
