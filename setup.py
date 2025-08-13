6e9b801c069933155cc3c3acf91021958d3a9582
Syed Muzakkir Hussain <138936198+muzakkirhussain011@users.noreply.github.com>
2025-08-14 04:52:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="faircare",
-    version="1.0.0",
-    description="FairCare-FL: Unified Fair Federated Learning for Healthcare",
-    author="Your Name",
-    packages=find_packages(exclude=("tests", "paper")),
-    include_package_data=True,
-    install_requires=[],
-    python_requires=">=3.9",
+    packages=find_packages(),
+    python_requires=">=3.8",
 )
