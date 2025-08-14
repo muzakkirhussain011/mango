650dafc34d4ec40a2f6eb76a15923ee38cba6ffe
Syed Muzakkir Hussain <138936198+muzakkirhussain011@users.noreply.github.com>
2025-08-14 22:01:31 +0530

    Create __init__.py

---
+++ faircare/models/__init__.py
@@ -0,0 +1,15 @@
+"""Neural network models for federated learning."""
+
+from faircare.models.classifier import (
+    MLP,
+    LogisticRegression,
+    create_model
+)
+from faircare.models.adversary import AdversarialDebiaser
+
+__all__ = [
+    "MLP",
+    "LogisticRegression",
+    "create_model",
+    "AdversarialDebiaser"
+]
