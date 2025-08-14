49caae04a48986d42e45fd67baf1d52c3ba0f68f
Syed Muzakkir Hussain <138936198+muzakkirhussain011@users.noreply.github.com>
2025-08-14 22:03:34 +0530

    Update __init__.py

--- faircare/__init__.py
+++ faircare/__init__.py
@@ -2,9 +2,11 @@
 
 __version__ = "0.1.0"
 
-from faircare.core import Client, Server
-from faircare.algos import REGISTRY as ALGO_REGISTRY
-from faircare.fairness import fairness_report, group_confusion_counts
+# Import after submodule init files are loaded
+from faircare.core.client import Client
+from faircare.core.server import Server
+from faircare.algos.aggregator import REGISTRY as ALGO_REGISTRY
+from faircare.fairness.metrics import fairness_report, group_confusion_counts
 
 __all__ = [
     "Client",
