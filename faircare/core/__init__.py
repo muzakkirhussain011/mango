9e62c7eb858cb0f0f98e9f7e3a14268c40cecf49
Syed Muzakkir Hussain <138936198+muzakkirhussain011@users.noreply.github.com>
2025-08-14 22:00:51 +0530

    Create __init__.py

---
+++ faircare/core/__init__.py
@@ -0,0 +1,33 @@
+"""Core federated learning components."""
+
+from faircare.core.client import Client
+from faircare.core.server import Server
+from faircare.core.secure_agg import SecureAggregator
+from faircare.core.trainer import run_experiment
+from faircare.core.evaluation import Evaluator
+from faircare.core.utils import (
+    set_seed,
+    seed_context,
+    Logger,
+    sample_clients,
+    average_weights,
+    weighted_average_weights,
+    compute_model_delta,
+    apply_model_delta
+)
+
+__all__ = [
+    "Client",
+    "Server",
+    "SecureAggregator",
+    "run_experiment",
+    "Evaluator",
+    "set_seed",
+    "seed_context",
+    "Logger",
+    "sample_clients",
+    "average_weights",
+    "weighted_average_weights",
+    "compute_model_delta",
+    "apply_model_delta"
+]
