b3f4b612c8e65234aa26214a8951576ccf76ffe1
Syed Muzakkir Hussain <138936198+muzakkirhussain011@users.noreply.github.com>
2025-08-14 23:24:21 +0530

    Update __init__.py

--- faircare/algos/__init__.py
+++ faircare/algos/__init__.py
@@ -1,22 +1,9 @@
-
-"""Federated learning algorithms."""
+"""Federated learning algorithms (lightweight export for tests)."""
 
 from faircare.algos.aggregator import Aggregator, REGISTRY, make_aggregator
-from faircare.algos.fedavg import FedAvgAggregator
-from faircare.algos.fedprox import FedProxAggregator
-from faircare.algos.qffl import QFFLAggregator
-from faircare.algos.afl import AFLAggregator
-from faircare.algos.fairfate import FairFateAggregator
-from faircare.algos.fairfed import FairFedAggregator
 
 __all__ = [
     "Aggregator",
     "REGISTRY",
     "make_aggregator",
-    "FedAvgAggregator",
-    "FedProxAggregator",
-    "QFFLAggregator",
-    "AFLAggregator",
-    "FairFateAggregator",
-    "FairFedAggregator"
 ]
